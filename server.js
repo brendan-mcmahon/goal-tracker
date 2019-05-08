1b3af67f31afbfb88392f6520eda5408d94c19a6
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2019-05-08 08:46:01 -0400

    update it again (oops)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/goal-trackr'));
+app.use(express.static(__dirname + '/dist/goal-tracker'));
 
 app.get('/*', function(req,res) {
 
