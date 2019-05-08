85c1fc3b044a70f5333ac774cba6014967bf6530
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2019-05-08 07:33:55 -0400

    fixed config.js

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
-const fs = require("fs");
-const apiKey = process.env.firebaseApiKey,
-const authDomain = process.env.firebaseAuthDomain,
-const databaseURL = process.env.firebaseDatabaseURL,
-const projectId = process.env.firebaseProjectId,
-const storageBucket = process.env.firebaseStorageBucket,
-const messagingSenderId = process.env.firebaseMessagingSenderId
+import { readFileSync, writeFileSync } from "fs";
 
-const configFile = fs.readFileSync('./src/environments/environment.prod.ts', 'utf-8');
+const apiKey = process.env.firebaseApiKey;
+const authDomain = process.env.firebaseAuthDomain;
+const databaseURL = process.env.firebaseDatabaseURL;
+const projectId = process.env.firebaseProjectId;
+const storageBucket = process.env.firebaseStorageBucket;
+const messagingSenderId = process.env.firebaseMessagingSenderId;
+
+const configFile = readFileSync('./src/environments/environment.prod.ts', 'utf-8');
 const appURL = `URL: 'https://${appName}.herokuapp.com',`;
 const newValue = configFile.replace('{apiKey}', apiKey);
 const newValue = configFile.replace('{authDomain}', authDomain);
@@ -15,4 +16,4 @@ const newValue = configFile.replace('{projectId}', projectId);
 const newValue = configFile.replace('{storageBucket}', storageBucket);
 const newValue = configFile.replace('{messagingSenderId}', messagingSenderId);
 
-fs.writeFileSync('./src/environments/environment.prod.ts', newValue, 'utf-8');
+writeFileSync('./src/environments/environment.prod.ts', newValue, 'utf-8');
